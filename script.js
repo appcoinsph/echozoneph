d8daa828a15e02e2ced47ce4982edff9c2ff8f9d
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-11-13 19:49:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ toggle.addEventListener('click' , function(){
     overlay.classList.toggle('overlay')
     after.classList.toggle('after-effect')
 })
+
+var loader = await Notix.Interstitial.createLoader(/*6601018*/);
+loader.startLoading();
+InterstitialData interstitialData;
+try {
+    interstitialData = await loader.next();
+} catch (e) {
+    /* handle exception */
+}
+Notix.Interstitial.show(interstitialData);
