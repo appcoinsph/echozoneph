073c9c4efe26bff7d73e3db9abeee8ab9c73b539
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-02-19 08:54:15 +0800

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -3,14 +3,14 @@
 
     if (!isset($_SESSION['luser'])) {
         echo "Please Login again";
-        echo "<a href='http://localhost/somefolder/login.php'>Click Here to Login</a>";
+        echo "<a href='login.php'>Click Here to Login</a>";
     }
     else {
         $now = time(); // Checking the time now when home page starts.
 
         if ($now > $_SESSION['expire']) {
             session_destroy();
-            echo "Your session has expired! <a href='http://localhost/somefolder/login.php'>Login here</a>";
+            echo "Your session has expired! <a href='login.php'>Login here</a>";
         }
         else { //Starting this else one [else1]
 ?>
@@ -19,7 +19,7 @@
                 Welcome
                 <?php
                     echo $_SESSION['luser'];
-                    echo "<a href='http://localhost/somefolder/logout.php'>Log out</a>";
+                    echo "<a href='logout.php'>Log out</a>";
                 ?>
             </html>
 <?php
